fa22e44c11cae730574b679d7475cc2b39141e66
Walter2054 <73790667+Walter2054@users.noreply.github.com>
2023-01-31 23:38:46 +0200

    Add project files.

---
+++ For, Sillaste Lauri/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace For6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Sisesta, kui palju korrutuste arvu kümnatega te soovite näha: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                double result = Math.Pow(10, i);
+                Console.WriteLine(result);
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Arvutamise lõpp");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+
